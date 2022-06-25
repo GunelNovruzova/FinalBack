3087210bb21224f4fcc7846f5ea97d2311cee11d
GunelNovruzova <gunelvn@code.edu.az>
2022-06-25 17:37:31 +0400

    TableChanges

--- Final/Controllers/TableController.cs
+++ Final/Controllers/TableController.cs
@@ -32,40 +32,30 @@ namespace Final.Controllers
         }
         public async Task<IActionResult> TableReservation(Table table,string from) 
         {
-
             if (!User.Identity.IsAuthenticated)
             {
                 return RedirectToAction("login", "account");
             }
-            
-
             AppUser appUser = await _userManager.Users.FirstOrDefaultAsync(u => u.UserName == User.Identity.Name && !u.IsAdmin);
-
-
             if (string.IsNullOrWhiteSpace(table.Name))
             {
                 ModelState.AddModelError("Name", "There should be no gaps");
                 return RedirectToAction("index");
             }
-
             if (string.IsNullOrWhiteSpace(table.Email))
             {
                 ModelState.AddModelError("Email", "There should be no gaps");
                 return RedirectToAction("index");
             }
-
             if (await _context.Tables.AnyAsync(t=>t.Date>=table.Date.AddHours(-1)&&t.Date<=table.Date.AddHours(1)))
             {
                 ModelState.AddModelError("", "Yerimiz doludu qardas :D duz deyir");
                 TempData["error"] = "Yerimiz doludu qardas :D duz deyir";
             }
             table.MainEmail = appUser.Email; 
-
             table.CreatedAt = DateTime.UtcNow.AddHours(4);
-
             await _context.Tables.AddAsync(table);
             await _context.SaveChangesAsync();
-
             if (from=="home")
             {
                 return RedirectToAction("index", "home");
@@ -73,9 +63,7 @@ namespace Final.Controllers
             else
             {
                 return RedirectToAction("index","table");
-            }
-            
+            }  
         }
-
     }
 }
