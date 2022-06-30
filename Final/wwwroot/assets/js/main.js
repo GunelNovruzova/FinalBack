68d76e91bf32379ea9e13fe277cc24fbbf8769c6
GunelNovruzova <gunelvn@code.edu.az>
2022-06-30 18:17:43 +0400

    resp

--- Final/wwwroot/assets/js/main.js
+++ Final/wwwroot/assets/js/main.js
@@ -1,5 +1,4 @@
 $(document).ready(function () {
-    //slick
     $('.images').slick({
         slidesToShow: 4,
         slidesToScroll: 1,
@@ -30,56 +29,6 @@ $(document).ready(function () {
 		$(".ship-to-different").slideToggle("100");
 	});
 });
-//for header
-$(window).scroll(function () {
-    var header = $('.header'),
-        scroll = $(window).scrollTop();
-
-    if (scroll >= 150) {
-        header.css({
-            'position': 'fixed',
-            'top': '0',
-            'left': '0',
-            'right': '0',
-            'z-index': '99999'
-        });
-    } else {
-        header.css({
-             'position': 'absolute'
-            
-        });
-    }
-});
-
-
-//Blog Side
-
-//let commentForm = document.getElementById("CommentForm");
-
-//commentForm.addEventListener("submit", function (ev) {
-//    ev.preventDefault();
-//    console.log("Test");
-//})
-
-// to top function
-//btn
-document.querySelector(".topBtn").addEventListener("click", function () {
-    window.scrollTo({
-        top: 0,
-        behavior: 'smooth'
-    });
-})
-function ScrollToTop() {
-    let scBtn = document.querySelector(".toTopBtn");
-    if (window.scrollY > 250) {
-        scBtn.style.opacity = "1"
-        scBtn.style.visibility = "visible"
-    }
-    else {
-        scBtn.style.opacity = "0"
-        scBtn.style.visibility = "hidden"
-    }
-}
 AOS.init();
 
 
--- Final/wwwroot/assets/js/side-bar.js
+++ Final/wwwroot/assets/js/side-bar.js
@@ -1,7 +1,8 @@
 $(document).ready(function(){
 
-    $(".nav-bar").slideUp();
-
+    // $(".nav-bar").slideUp();
+      $(".nav-bar").css({ "display": "none" });
+    $(".side-menu").removeClass("side-menu-active");
     $(".side-menu").click(function(){
         $(this).toggleClass("side-menu-active");
         $(".nav-bar").slideToggle();
